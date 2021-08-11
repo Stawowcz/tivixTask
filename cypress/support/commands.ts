676b93af861e878bd0a538f41a280311bde4767e
Jakub Stawowczyk <jakub.stawowczyk@almservices.pl>
2021-08-11 22:04:11 +0200

    migration to ts

---
+++ cypress/support/commands.ts
@@ -0,0 +1,16 @@
+Cypress.Commands.add('searchCarToRent', (country, city, model, pickup, dropoff) => {
+    cy.get('#country').select(country)
+    cy.get('#city').select(city)
+    cy.get('#model').type(model)
+    cy.get('#pickup').type(pickup)
+    cy.get('#dropoff').type(dropoff)
+    cy.get('[type="submit"]').click()
+  });
+
+Cypress.Commands.add('fillClientPersonalData', (name, surname, cartNumber, email) => {
+    cy.get('#name').type(name)
+    cy.get('#last_name').type(surname)
+    cy.get('#card_number').type(cartNumber)
+    cy.get('#email').type(email)
+  });
+
